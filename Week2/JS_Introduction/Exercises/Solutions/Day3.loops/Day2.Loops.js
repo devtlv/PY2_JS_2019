ae420e3b023d92b75851aee8ee365708135deacd
LiseNakache <nakache.lise@gmail.com>
2019-11-05 11:41:06 +0200

    Week 2

---
+++ Week2/JS_Introduction/Exercises/Solutions/Day3.loops/Day2.Loops.js
@@ -0,0 +1,12 @@
+var names= ["john", "sarah", 23, "Rudolf",34]
+for (var i = 0; i < names.length; i++) {
+	if (typeof names[i] !== "string") {
+		continue;
+    } 
+        if (names[i].charAt(0).toUpperCase() == names[i][0]){
+            console.log(names[i])
+        } else {
+			names[i] = names[i][0].toUpperCase() + names[i].substring(1,)
+			console.log(names[i])
+        }
+}
